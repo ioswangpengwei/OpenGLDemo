80ea979c8ff7bd31bb6a4b2b7809db4a63224c40
pengweiwang <ioswangpengwei@163.com>
2020-09-09 16:58:14 +0800

    add

---
+++ objc4-787.1/TestOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestOC
+//
+//  Created by MacW on 2020/9/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        [NSObject new];
+    }
+    return 0;
+}
